3c26d0bb7e17f89c76479be07b0b7796edc77f67
Christiano0407 <placebo0704@gmail.com>
2020-09-24 13:02:09 -0500

    logo-vid

---
+++ src/js/videoApp.js
@@ -0,0 +1,7 @@
+{/* <script>
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('scrolling-active', windowPosition);
+})
+</script> */}
