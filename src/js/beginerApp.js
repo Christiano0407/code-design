9fe95bbc6bd44018457cde2cd9f232ec99073f4c
Christiano0407 <placebo0704@gmail.com>
2020-09-24 09:07:14 -0500

    js2

--- src/js/beginerApp.js
+++ src/js/beginerApp.js
@@ -8,18 +8,26 @@ todoButton.addEventListener(`click`, addtodo);
 
 /* Functions */
 function addtodo(event) {
+    /* PREVENT FORM FROM SUBTIMMING */
     event.preventDefault();
-
+   /*  TODO DIV */
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
-
+   /*  CREATE LI  */
     const newTodo = document.createElement(`li`);
     newTodo.innerText = `hey`;
     newTodo.classList.add(`todo-item`);
     todoDiv.appendChild(newTodo);
-
+    /* CHECK MARK BUTTON */
     const completedButton = document.createElement(`button`);
-    completedButton.innerText = `<i class="fas fa-check"></i>`;
+    completedButton.innerText = `<i class=fas fa-check"></i>`;
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
+   /*  CHECK TRASH BUTTON */
+    const trashButton = document.createElement(`button`);
+    trashButton.innerText = `<i class="fas fa-trash"></i>`;
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+  /*    APPEND TO LIST */
+    todoList.appendChild(todoDiv);
 }
