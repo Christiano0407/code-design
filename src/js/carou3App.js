2055f9a91035309eb3429b970f4dbccab642fa8e
Christiano0407 <placebo0704@gmail.com>
2020-09-30 09:31:12 -0500

    carou-3

---
+++ src/js/carou3App.js
@@ -0,0 +1,29 @@
+/* window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__list'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicators',
+		arrows: {
+			prev: '.carousel__previous',
+			next: '.carousel__next'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+}); */
