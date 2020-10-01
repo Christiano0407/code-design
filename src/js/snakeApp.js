c6258b76d78a98380c4460eac1b22fdc4b94b80a
Christiano0407 <placebo0704@gmail.com>
2020-10-01 08:39:47 -0500

    interval

--- src/js/snakeApp.js
+++ src/js/snakeApp.js
@@ -16,6 +16,23 @@ document.addEventListener(`DOMContentLoaded`, () => {
 })
 
 // To start, and restart the game
+function startGame(){
+    currentSnake.forEach(index => squares[index].classList.remove(`snake`))
+    squares[appleIndex].classList.remove(`apple`)
+    clearInterval(interval)
+    score = 0
+    //randomApple()
+    direction = 1
+    scoreDisplay.innerText = score
+    intervalTime = 1000
+    currentSnake[2,1,0]
+    currentIndex = 0
+    currentSnake.forEach(index => squares[index].classList.add(`snake`))
+    interval = setInterval(moveOutcomes, intervalTime)
+}
+
+//function that deals with all the ove outcomes of the snake
+
 
 //assign function  to keycode
 function control(e) {
