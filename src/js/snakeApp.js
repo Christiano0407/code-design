8e1fb94802a268d4e0d003d6df86d939321ed31a
Christiano0407 <placebo0704@gmail.com>
2020-10-01 08:21:24 -0500

    start-game

--- src/js/snakeApp.js
+++ src/js/snakeApp.js
@@ -1,11 +1,39 @@
+//Detector de eventos
 document.addEventListener(`DOMContentLoaded`, () => {
     const squares = document.querySelectorAll(`.grid div`);
     const scoreDisplay = document.querySelector(`span`);
     const startBtn = document.querySelector(`.start`);
 
    const width = 10;
+   let currentIndex = 0 // so first div in our grid
+   let appleIndex = 0 // so first div in our grid
+   let currentSnake = [2,1,0]
+   let direction = 1
+   let score = 0
+   let speed = 0.9
+   let intervalTime = 0
+   let interval = 0
 })
 
+// To start, and restart the game
+
+//assign function  to keycode
+function control(e) {
+    squares[currentIndex].classList.remove(`snake`)
+
+    if(e.keyCode === 39){
+        direction = 1 //if we press right arrow on our keyboard, the class of snake from all the squares.
+    }else if (e.keyCode === 38){
+        direction = -width //if we press the up arrow, the snake will go back ten divs, appearing to go up.
+    }else if(e.keyCode === 37) {
+        direction -1 //if we press left, the snake will go left one div.
+    }else if (e.keyCode === 40 ) {
+        direction = +width //if we press down, the snake head will appear in the div from where you are now
+    }
+
+    document.addEventListener(`keyup`, control)
+    startBtn.addEventListener(`click`, startGame)
+}
 
 
 /* ORIGINAL ----------------- */
